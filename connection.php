8fdc43d0b89886621597dab1fd5ed5568c209181
pedjaslavkovic <134131457+pedjaslavkovic@users.noreply.github.com>
2023-07-17 16:58:26 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 Class dbObj{
 	/* Database connection start */
-	var $servername = "localhost";
-	var $username = "id21019371_intedlwb_kristian";
-	var $password = "Pedja1234!";
-	var $dbname = "id21019371_intedlwb_coiffeur";
+	var $servername = "";
+	var $username = "";
+	var $password = "";
+	var $dbname = "";
 	var $conn;
 	function getConnstring() {
 		$con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error());
